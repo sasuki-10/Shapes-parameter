1cf3d9ab3acdb401133dd6fc3c73fbde3442a277
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-11-12 23:02:50 +0530

    Area of triangle.c

---
+++ Area of triangle.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+void area(int a,int b)
+{
+  int s;
+  s=(a*b)/2;
+  printf("Area of triangle:%d",s);
+  
+}
+void perimeter(int a,int b)
+{
+	int sides;
+	int p;
+	printf("\nEnter no. of sides which are equal....:");
+	scanf("%d",&sides);
+	if(sides==3)
+	{
+		p=3*a;
+		printf("\nTRIANGLE IS EQUILATERAL...");
+		printf("\nperimeter is:%d",p);
+	}
+	else if(sides==2)
+	{
+		p=2*a+b;
+	    printf("\nTRIANGLE IS ISOSCALES...");
+		printf("\nperimeter is:%d",p);	
+	}
+}
+int main()
+{
+	int a,b;
+	printf("enter the value of Base:");
+	scanf("%d",&a);
+	printf("\nenter the value of height:");
+	scanf("%d",&b);
+	area(a,b);
+	perimeter(a,b);
+}
