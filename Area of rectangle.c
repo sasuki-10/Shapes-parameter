23fad30423b4d0d44769c43995f8cfdcae59b0c5
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-11-12 23:27:39 +0530

    Area of rectangle.c

---
+++ Area of rectangle.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void area(int a,int b)
+{
+  int s;
+  s=a*b;
+  printf("\nArea of Rectangle:%d",s);
+}
+void perimeter(int a,int b)
+{
+	int p;
+	p=2*(a+b);
+	printf("\nPerimeter of Rectangle:%d",p);
+}
+
+int main()
+{
+	int a,b;
+	printf("Enter the value of Base:");
+	scanf("%d",&a);
+	printf("\nEnter the value of height:");
+	scanf("%d",&b);
+	area(a,b);
+	perimeter(a,b);
+}
